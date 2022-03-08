95ee9f299217c158ec0c4d69003b1edf82c80712
Lalit Puri <laleetpuree5768@gmail.com>
2022-03-08 15:12:24 +0530

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,37 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
+import { useState } from "react";
 
 export default function Home({ countries }) {
   console.log(countries);
+
+  const [keyword, setKeyword] = useState("");
+  const filterCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
   return (
     <Layout>
-      <div className={styles.counts}>Found {countries.length} countries</div>
-      <SearchInput placeholder="Filter by country, Region or subRegion" />
-      <CountriesTable countries={countries} />
+      <div className={styles.input_container}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by country, Region or subRegion"
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
+
+      <CountriesTable countries={filterCountries} />
     </Layout>
   );
 }
